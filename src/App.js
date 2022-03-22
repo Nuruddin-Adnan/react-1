500a6a34197a999bce2976ce58b4366abdcc428a
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-03-22 20:03:57 +0600

    form handling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import ClockList from './component/ClockList';
-import Counter from './component/Counter';
+// import ClockList from './component/ClockList';
+// import Counter from './component/Counter';
+import Form from './component/Form';
 
 function App() {
-  console.log('App compent render');
   const quantities = [1, 2, 3];
   return (
     <div className="App">
-      <ClockList quantities={quantities} />
-      <Counter />
+      {/* <ClockList quantities={quantities} />
+      <Counter /> */}
+      <Form />
     </div>
   );
 }
